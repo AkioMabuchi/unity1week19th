9d0a3561dbfa7466c0009c4601dc293d599f6d73
AkioMabuchi <mabuchiakio@gmail.com>
2021-04-25 22:31:53 +0900

    First Commit

---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Type t = typeof(T);
+                _instance = (T) FindObjectOfType(t);
+                if (_instance == null)
+                {
+                    Debug.LogError(t + "をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        CheckInstance();
+    }
+
+    void CheckInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            return;
+        }
+
+        if (_instance == this)
+        {
+            return;
+        }
+
+        Destroy(this);
+    }
+}
